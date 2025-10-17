43ce8f7c13e29ed7377dbb88415a1634de67297b
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-17 19:26:11 +0600

    Update: login and registration api json response success

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,6 +41,7 @@ class AuthController extends Controller
         }
 
         return response()->json([
+            'message' => "Login successful",
             'token' => $token,
             'token_type' => 'bearer',
             'expires_in' => JWTAuth::factory()->getTTL() * 60
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
