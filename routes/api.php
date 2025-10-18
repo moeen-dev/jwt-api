f047cd04920b2213a58812bd7adf4a7419873f52
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-18 21:50:32 +0600

    Update: configure api.php apiResource route

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
+    // User route
     Route::get('me', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
+
+    // Product route
+    Route::apiResource('products', ProductController::class);
 });
